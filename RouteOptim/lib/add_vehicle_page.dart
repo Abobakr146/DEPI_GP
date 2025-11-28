348f6c76919c46d11f9f4b69d2ebe6d74bd58bd3
Abobakr146 <abobakr.abdelrahman45@gmail.com>
2025-11-28 15:38:27 +0200

    feat: Add vehicle service and integrate vehicle addition in AddVehiclePage

--- RouteOptim/lib/add_vehicle_page.dart
+++ RouteOptim/lib/add_vehicle_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:route_optim/vehicle.dart';
+import 'package:route_optim/vehicle_service.dart';
 
 class AddVehiclePage extends StatefulWidget {
   const AddVehiclePage({super.key});
@@ -16,6 +17,8 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
 
   final fuelTypeController = TextEditingController();
   var fuelType;
+
+  final vehicleService = VehicleService();
 
   @override
   void dispose() {
@@ -43,138 +46,138 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
         ),
       ),
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                spacing: 8,
-                children: [
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        spacing: 8,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Vehicle Name'),
-                          TextField(
-                            controller: vehicleNameController,
-                            decoration: const InputDecoration(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 8,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      spacing: 8,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Vehicle Name'),
+                        TextField(
+                          controller: vehicleNameController,
+                          decoration: const InputDecoration(
                               hintText: 'Enter vehicle name',
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
                               )
-                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        spacing: 8,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Engine CC Range'),
-                          DropdownMenu<double>(
-                            width: double.infinity,
-                            controller: ccRangeController,
-                            label: const Text('Select CC Range'),
-                            menuStyle: MenuStyle(
-                              maximumSize: WidgetStateProperty.all<Size>(
-                                const Size(300, 400),
-                              ),
-                              // backgroundColor: WidgetStateProperty.all<Color>(Colors.white10)
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      spacing: 8,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Engine CC Range'),
+                        DropdownMenu<double>(
+                          width: double.infinity,
+                          controller: ccRangeController,
+                          label: const Text('Select CC Range'),
+                          menuStyle: MenuStyle(
+                            maximumSize: WidgetStateProperty.all<Size>(
+                              const Size(300, 400),
                             ),
-                            inputDecorationTheme: InputDecorationTheme(
-                              filled: true,
-                              fillColor: const Color(0xFFF3F3F5),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: const BorderSide(color: Colors.grey),
-                              ),
-                            ),
-                            dropdownMenuEntries: const [
-                              DropdownMenuEntry(value: 5, label: '500-1000 cc'),
-                              DropdownMenuEntry(value: 6.75, label: '1000-1500 cc'),
-                              DropdownMenuEntry(value: 8.5, label: '1500-2000 cc'),
-                              DropdownMenuEntry(value: 10.25, label: '2000-2500 cc'),
-                              DropdownMenuEntry(value: 11.75, label: '2500-3000 cc'),
-                              DropdownMenuEntry(value: 13.5, label: '3000-3500 cc'),
-                              DropdownMenuEntry(value: 15.5, label: '3500-4000 cc'),
-                            ],
-                            onSelected: (double? value) {
-                              consumption = value;
-                              // ccRangeController.text = ;
-                              print('Selected consumption: $consumption');
-                              print('Controller text: ${ccRangeController.text}');
-                            },
+                            // backgroundColor: WidgetStateProperty.all<Color>(Colors.white10)
                           ),
-                        ],
-                      ),
+                          inputDecorationTheme: InputDecorationTheme(
+                            filled: true,
+                            fillColor: const Color(0xFFF3F3F5),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(color: Colors.grey),
+                            ),
+                          ),
+                          dropdownMenuEntries: const [
+                            DropdownMenuEntry(value: 5, label: '500-1000 cc'),
+                            DropdownMenuEntry(value: 6.75, label: '1000-1500 cc'),
+                            DropdownMenuEntry(value: 8.5, label: '1500-2000 cc'),
+                            DropdownMenuEntry(value: 10.25, label: '2000-2500 cc'),
+                            DropdownMenuEntry(value: 11.75, label: '2500-3000 cc'),
+                            DropdownMenuEntry(value: 13.5, label: '3000-3500 cc'),
+                            DropdownMenuEntry(value: 15.5, label: '3500-4000 cc'),
+                          ],
+                          onSelected: (double? value) {
+                            consumption = value;
+                            // ccRangeController.text = ;
+                            print('Selected consumption: $consumption');
+                            print('Controller text: ${ccRangeController.text}');
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        spacing: 8,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Fuel Type'),
-                          DropdownMenu<String>(
-                            width: double.infinity,
-                            controller: fuelTypeController,
-                            label: const Text('Select Fuel Type'),
-                            menuStyle: MenuStyle(
-                              maximumSize: WidgetStateProperty.all<Size>(
-                                const Size(300, 400),
-                              ),
-                              // backgroundColor: WidgetStateProperty.all<Color>(Colors.white10)
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      spacing: 8,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Fuel Type'),
+                        DropdownMenu<String>(
+                          width: double.infinity,
+                          controller: fuelTypeController,
+                          label: const Text('Select Fuel Type'),
+                          menuStyle: MenuStyle(
+                            maximumSize: WidgetStateProperty.all<Size>(
+                              const Size(300, 400),
                             ),
-                            inputDecorationTheme: InputDecorationTheme(
-                              filled: true,
-                              fillColor: const Color(0xFFF3F3F5),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: const BorderSide(color: Colors.grey),
-                              ),
-                            ),
-                            dropdownMenuEntries: const [
-                              DropdownMenuEntry(value: 'Gasoline 80', label: 'Gasoline 80'),
-                              DropdownMenuEntry(value: 'Gasoline 92', label: 'Gasoline 92'),
-                              DropdownMenuEntry(value: 'Gasoline 95', label: 'Gasoline 95'),
-                              DropdownMenuEntry(value: 'Solar', label: 'Diesel - Solar'),
-                            ],
-                            onSelected: (String? value) {
-                              fuelType = value;
-                            },
+                            // backgroundColor: WidgetStateProperty.all<Color>(Colors.white10)
                           ),
-                        ],
-                      ),
+                          inputDecorationTheme: InputDecorationTheme(
+                            filled: true,
+                            fillColor: const Color(0xFFF3F3F5),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(color: Colors.grey),
+                            ),
+                          ),
+                          dropdownMenuEntries: const [
+                            DropdownMenuEntry(value: 'Gasoline 80', label: 'Gasoline 80'),
+                            DropdownMenuEntry(value: 'Gasoline 92', label: 'Gasoline 92'),
+                            DropdownMenuEntry(value: 'Gasoline 95', label: 'Gasoline 95'),
+                            DropdownMenuEntry(value: 'Solar', label: 'Diesel - Solar'),
+                          ],
+                          onSelected: (String? value) {
+                            fuelType = value;
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  ElevatedButton(
-                      onPressed: (){
-                        // TODO: Save vehicle logic and clear fields
-                        print('Vehicle Name: ${vehicleNameController.text}');
-                        print('Consumption: $consumption L/100km');
-                        print('Fuel Type: $fuelType');
-                        final vehicle = Vehicle(
-                            manufacture: vehicleNameController.text,
-                            consumption: consumption,
-                            fuelType: fuelType
-                        );
-
-                      },
-                      child: const Text('Save Vehicle')
-                  ),
-                ],
-              ),
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      // TODO: Save vehicle logic and clear fields
+                      print('Vehicle Name: ${vehicleNameController.text}');
+                      print('Consumption: $consumption L/100km');
+                      print('Fuel Type: $fuelType');
+                      final vehicle = Vehicle(
+                          manufacture: vehicleNameController.text,
+                          consumption: consumption,
+                          fuelType: fuelType
+                      );
+                      vehicleService.addVehicle(vehicle);
+                    },
+                    child: const Text('Save Vehicle')
+                ),
+              ],
             ),
           ),
+        ),
       ),
     );
   }
--- RouteOptim/lib/vehicle_service.dart
+++ RouteOptim/lib/vehicle_service.dart
@@ -8,8 +8,16 @@ import 'main.dart';
 class VehicleService extends GetxService {
   Future<List<Vehicle>> loadAllVehicles() async {
     print('Loading all vehicles from database...');
-    var response = await cloud.from('Vehicle').select('manufacture, fuel_type, consumption').eq('id', user!.id);
+    final response = await cloud.from('Vehicle').select('manufacture, fuel_type, consumption').eq('user_id', user!.id);
+    print(response);
+    print(json.encode(response));
     final vehicles = vehicleFromJson(json.encode(response));
     return vehicles;
+  }
+
+  Future<void> addVehicle(Vehicle vehicle) async {
+    print('Adding vehicle to database...');
+    final response = await cloud.from('Vehicle').insert(vehicle.toJson());
+    print(response);
   }
 }
